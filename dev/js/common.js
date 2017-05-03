a1eb24114ce1941236f246e2a1c28d92f74ab9cd
polomanij <polomanijmail@gmail.com>
2017-05-03 07:00:15 +0300

    03.05.2017. 7:00

---
+++ dev/js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(".toggle-menu").on("click", function() {
+    $(".menu").slideToggle(500);
+    $(".menu li").css("display", "block");
+    return false;
+  });
+  
+  $(window).resize(function() {
+    
+    if($(window).width() > 500) {
+      $(".menu").removeAttr("style");
+    }
+    
+  });
+  
+})
